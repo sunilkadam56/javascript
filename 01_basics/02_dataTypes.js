b58f46ca596fa5a935de276ac6837320b4ba058d
sunilkadam56 <105190409+sunilkadam56@users.noreply.github.com>
2024-03-10 17:36:53 +0000

    dataType Summary added

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3+3)  // we are using node.js not browser
+
+console.log(3 + 3);  // code readability should be high
+
+let name = "Sunil"
+let age = 32
+let isLoggedIn = false
+
+console.table([name, age, isLoggedIn]);
+//number 2 to power 53
+//null=> is a standalone value
+//undefined =>
+//String => ""
+//bigint
+//symbole => unique
+
+console.log(typeof null); //for check which is used data type
+
+
+
+
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,59 @@
+/* number conversion
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber  = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"  => 33
+//"33abc" => NaN
+//true =1; false = 0;
+
+//let isLoggedIn 
+let isLoggedIn =""
+//let isLogged ="Sunil"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+*/
+
+
+// *************** Operations *****************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+console.log("1" + 2);  //12
+console.log(1+"2");    //12
+console.log("1" + 2 + 2); //122
+console.log(1+2+"2");  //32
+
+console.log(+true); //1    true+ not allowed
+
+
+let gameCounter = 100
+//++gameCounter;
+console.log(gameCounter); //101
+
+gameCounter++;
+console.log(gameCounter);
+
