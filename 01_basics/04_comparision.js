83eb79965cfda14dd889c730e5c05992b03eb79c
sunilkadam56 <105190409+sunilkadam56@users.noreply.github.com>
2024-03-10 17:38:17 +0000

    data type summary

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -4,3 +4,11 @@ console.log(2==1);
 console.log(2>=1);
 console.log(2!=1);
 
+console.log("*****************");
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null == 0);
+
+console.log("*************");
+console.log("2" === 2);
---
+++ 01_basics/05_dataTypes-summary.js
@@ -0,0 +1,16 @@
+//Primitive 
+
+// 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt, 
+
+
+// Reference (No primitive)
+
+//3 types Array, objects, Functions.
+
+const s = 100
+const sf = 100.5
+const isloggedIn = false
+
+const id = Symbol("123")
+
+const bigNumber = 51325545154124514153n
